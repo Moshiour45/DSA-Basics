c307ad89c595f38709f3dcdcbbd7efcc3499161d
Moshiour45 <moshiourrahman145@gmail.com>
2025-12-10 01:22:43 +0600

    Bubble sort logic changed a bit.

--- Sorting Algorithms/BubbleSort.cpp
+++ Sorting Algorithms/BubbleSort.cpp
@@ -23,12 +23,17 @@ to place each largest or smallest element of the array at the end of the array. 
 void bubbleSort(vector<int> &v){
     int n = v.size();
     for(int i = 0; i < (n - 1); i++){
+        bool swapped = false;
         for(int j = 0; j < (n - i - 1); j++){
 
             // Here we will the opposite of what we want
             if(v[j] >= v[j + 1]){
+                swapped = true;
                 swap(v[j], v[j + 1]);
             }
+        }
+        if(!swapped){
+            return;
         }
     }
 }
