2586d1a8737dbe067a2787f4669a302fe6e2c59c
Moshiour45 <moshiourrahman145@gmail.com>
2026-02-28 15:26:32 +0600

    Recursive solution is converted into an iterative solution.

--- Dynamic Programming/MinimumSumPath.cpp
+++ Dynamic Programming/MinimumSumPath.cpp
@@ -60,6 +60,32 @@ public:
     }
 };
 
+
+// Iterative version of same problem : We can easily convert recurive solution to an iterative solution
+class Solution3 {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> dp(n, vector<int>(m));
+        dp[0][0] = grid[0][0];
+        // storing all the known answers in dp
+        for(int i = 1; i < n; i++){
+            dp[i][0] = grid[i][0] + dp[i - 1][0];
+        }
+        for(int j = 1; j < m; j++){
+            dp[0][j] = grid[0][j] + dp[0][j - 1];
+        }
+
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                dp[i][j] = grid[i][j] + min(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[n - 1][m - 1];
+    }
+};
+
 int main(){
     optimize();
     
