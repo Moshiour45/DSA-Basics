dc5f7d2b76017ca4d5efe9bb4c1c21b2148b3c19
Moshiour45 <moshiourrahman145@gmail.com>
2026-07-09 13:41:27 +0600

    Add a leetcode problem solution to the codebase

---
+++ Graph/RedundantConnection.cpp
@@ -0,0 +1,97 @@
+# include<bits/stdc++.h>
+# include<ext/pb_ds/assoc_container.hpp>
+# include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+# define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+# define endl '\n'
+
+
+// TC: O(1)
+class DSU{
+    vector<int> parent, size, rank;
+public:
+    DSU(int n){
+        for(int i = 0; i <= n; i++){
+            parent.push_back(i);
+            size.push_back(1);
+            rank.push_back(0);
+        }    
+    }
+
+    int findParent(int node){
+        if(parent[node] == node){
+            return node;
+        }
+        return parent[node] = findParent(parent[node]); // Path compression
+    }
+    
+    void unionBySize(int u, int v){
+        u = findParent(u); // Ultimate Parent of u in it's set
+        v = findParent(v); // Ultimate Parent of v in it's set
+        
+        // Both are from same set
+        if(u == v){
+            return;
+        }
+
+        // Connect Smaller to the Bigger set
+        if(size[u] < size[v]){
+            parent[u] = v;
+            size[v] += size[u];
+        }else{
+            parent[v] = u;
+            size[u] += size[v];
+        }
+    }
+
+    void unionByRank(int u, int v){
+        u = findParent(u); // Ultimate Parent of u in it's set
+        v = findParent(v); // Ultimate Parent of v in it's set
+        
+        // They are from same set
+        if(u == v){
+            return;
+        }
+
+        // Connect smaller rank to higher rank
+        if(rank[u] == rank[v]){
+            parent[v] = u;
+            rank[u]++;
+        }else if(rank[u] > rank[v]){
+            parent[v] = u;
+        }else{
+            parent[u] = v;
+        }
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        vector<vector<int>> ans;
+        DSU dsu(edges.size());
+        for(auto u : edges){
+            if(dsu.findParent(u[0]) != dsu.findParent(u[1])){
+                dsu.unionByRank(u[0], u[1]);
+            }else{
+                ans.push_back({u[0], u[1]});
+            }
+        }
+        return ans.back();      
+    }
+};
+
+int main(){
+    optimize();
+    
+    return 0;
+}
+
+// Problem : Redundant Connection (https://leetcode.com/problems/redundant-connection/description/)
